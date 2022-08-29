034107a3b291c36b77fb8031a7f6063de348deb3
geraldo sousa <geraldosousadev@gmail.com>
2022-08-28 23:43:08 -0300

    picking up new image name

--- app.py
+++ app.py
@@ -1,11 +1,37 @@
+import os
 from flask import Flask, render_template, request
-# from datetime import datetime
+from datetime import datetime
+from PIL import Image
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template('index.html')
+    if request.method == 'POST':
+        img = request.files['img']
+        options = request.form['options']
+        name = request.form['name']
+        
+        if img and (options  != 'escolha'):
+            new_image = Image.open(img, 'r')
+
+            if options == 'png':
+                
+                new_image.save(name + ".png")
+            elif options == 'jpg':
+                new_image.save(name + ".jpg")
+            elif options == 'jpeg':
+                new_image.save(name + ".jpeg")
+            else:
+                msg = 'invalid format'
+                return render_template('index.html', msg=msg)
+                
+        msg = 'successfully converted'
+        return render_template('index.html', msg=msg)
+    
+    success = "choose image and format"
+    return render_template('index.html', success=success)
+
 
 if __name__=='__main__':
     app.run()
